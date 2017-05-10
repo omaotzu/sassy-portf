cbf88066a57b669505d600653ad2d0f987c59d93
omaotzu <omarhphillips@gmail.com>
2017-05-11 00:13:54 +0100

    added toggle content visibility restructured layout

--- public/js/app.js
+++ public/js/app.js
@@ -5,18 +5,53 @@ $(function () {
   var $links = $('nav a');
   var $header = $('header');
 
-  $links.on('click', scrollToSection);
-  $window.scroll(updateHeader).trigger('scroll');
+  var $title1 = $('.title1');
+  var $title2 = $('.title2');
+  var $title3 = $('.title3');
+  var $title4 = $('.title4');
+
+  var $content1 = $('.content1');
+  var $content2 = $('.content2');
+  var $content3 = $('.content3');
+  var $content4 = $('.content4');
 
   function scrollToSection() {
     var section = $(this).attr('href');
     $('body').animate({
-      scrollTop: $(section).offset().top - 100
+      scrollTop: $(section).offset().top - 94.5
     }, 1000, function () {
       if ($window.width() < 575) {
         $('.dropdown').slideToggle();
       }
     });
+  }
+
+  function makeVisisble1() {
+    $content1.toggleClass('visible');
+    $content2.removeClass('visible');
+    $content3.removeClass('visible');
+    $content4.removeClass('visible');
+  }
+
+  function makeVisisble2() {
+    $content2.toggleClass('visible');
+    $content1.removeClass('visible');
+    $content3.removeClass('visible');
+    $content4.removeClass('visible');
+  }
+
+  function makeVisisble3() {
+    $content3.toggleClass('visible');
+    $content1.removeClass('visible');
+    $content2.removeClass('visible');
+    $content4.removeClass('visible');
+  }
+
+  function makeVisisble4() {
+    $content4.toggleClass('visible');
+    $content1.removeClass('visible');
+    $content2.removeClass('visible');
+    $content3.removeClass('visible');
   }
 
   function updateHeader() {
@@ -29,4 +64,11 @@ $(function () {
       $header.removeClass('opaque');
     }
   }
+
+  $links.on('click', scrollToSection);
+  $window.scroll(updateHeader).trigger('scroll');
+  $title1.on('click', makeVisisble1);
+  $title2.on('click', makeVisisble2);
+  $title3.on('click', makeVisisble3);
+  $title4.on('click', makeVisisble4);
 });
--- src/js/app.js
+++ src/js/app.js
@@ -3,18 +3,56 @@ $(() => {
   const $links = $('nav a');
   const $header = $('header');
 
-  $links.on('click', scrollToSection);
-  $window.scroll(updateHeader).trigger('scroll');
+  const $title1 = $('.title1');
+  const $title2 = $('.title2');
+  const $title3 = $('.title3');
+  const $title4 = $('.title4');
+
+  const $content1 = $('.content1');
+  const $content2 = $('.content2');
+  const $content3 = $('.content3');
+  const $content4 = $('.content4');
+
+
 
   function scrollToSection() {
     const section = $(this).attr('href');
     $('body').animate( {
-      scrollTop: $(section).offset().top -100
+      scrollTop: $(section).offset().top -94.5
     }, 1000, () => {
       if ($window.width() < 575) {
         $('.dropdown').slideToggle();
       }
     });
+  }
+
+
+  function makeVisisble1(){
+    $content1.toggleClass('visible');
+    $content2.removeClass('visible');
+    $content3.removeClass('visible');
+    $content4.removeClass('visible');
+  }
+
+  function makeVisisble2(){
+    $content2.toggleClass('visible');
+    $content1.removeClass('visible');
+    $content3.removeClass('visible');
+    $content4.removeClass('visible');
+  }
+
+  function makeVisisble3(){
+    $content3.toggleClass('visible');
+    $content1.removeClass('visible');
+    $content2.removeClass('visible');
+    $content4.removeClass('visible');
+  }
+
+  function makeVisisble4(){
+    $content4.toggleClass('visible');
+    $content1.removeClass('visible');
+    $content2.removeClass('visible');
+    $content3.removeClass('visible');
   }
 
   function updateHeader() {
@@ -27,4 +65,11 @@ $(() => {
       $header.removeClass('opaque');
     }
   }
+
+  $links.on('click', scrollToSection);
+  $window.scroll(updateHeader).trigger('scroll');
+  $title1.on('click', makeVisisble1);
+  $title2.on('click', makeVisisble2);
+  $title3.on('click', makeVisisble3);
+  $title4.on('click', makeVisisble4);
 });
